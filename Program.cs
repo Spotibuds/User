a27f5430a18b1780ea64228f4cae374c19226c97
unknown <dadashov.tamerlan@gmail.com>
2025-08-05 15:03:10 +0400

    change port

--- Program.cs
+++ Program.cs
@@ -288,7 +288,7 @@ builder.Services.AddCors(options =>
     });
 });
 
-builder.WebHost.UseUrls($"http://0.0.0.0:5002");
+builder.WebHost.UseUrls($"http://0.0.0.0:80");
 
 var app = builder.Build();
 
