67e34dc65edd71750203d7373a9bab23973d4b5a
unknown <dadashov.tamerlan@gmail.com>
2025-09-04 13:25:39 +0400

    Fix port

--- Program.cs
+++ Program.cs
@@ -353,7 +353,7 @@ builder.Services.AddCors(options =>
     });
 });
 
-builder.WebHost.UseUrls($"http://0.0.0.0:5002");
+builder.WebHost.UseUrls($"http://0.0.0.0:80");
 
 var app = builder.Build();
 
